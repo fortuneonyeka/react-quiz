76ce53c0d4e72cb2cfea677f5d1c5e1e790f00e5
fortune onyeka <ihedfortune@gmail.com>
2025-01-13 06:21:08 +0100

    fetched api with useEffect, created reducer with the action types

--- src/App.js
+++ src/App.js
@@ -1,9 +1,73 @@
-import DateCounter from "./DateCounter";
+import { useEffect, useReducer } from "react";
+import DateCounter from "./components/DateCounter";
+import Header from "./components/Header";
+import Main from "./components/Main";
 
+
+const initialState = {
+  question:[],
+
+  // loading,error,ready,active,completed
+  status: "loading",
+}
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "dataReceived":
+      return{
+        ...state,
+        questions: action.payload,
+        status: "ready"
+      };
+
+      case "dataFailed":
+      return {
+        ...state, status: "error"
+      };
+
+      case "questionActive":
+        return {
+          ...state,
+          activeQuestionIndex: action.payload,
+          status: "active"
+        }
+      
+  
+    default:
+      throw new Error("Unknown action")
+  }
+}
 function App() {
+
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  useEffect(() => {
+    const fetchData = async() => {
+      try {
+        const res = await fetch("http://localhost:8000/questions")
+        if (!res.ok) {
+          throw new Error("Couldn't fetch data")
+        }
+        const data = await res.json()
+        console.log(data)
+        dispatch({type:"dataReceived", payload:data})
+        
+
+      } catch (error) {
+        dispatch({type:"dataFailed"})
+       
+      }
+    }
+
+    fetchData()
+  },[])
+
+  
   return (
-    <div className="App">
-      <DateCounter />
+    <div className="app">
+      {/* <DateCounter /> */}
+      <Header />
+
+      <Main />
     </div>
   );
 }
---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Main = ({children}) => {
+  return (
+    <main className='main'>
+      {children}
+      <p>1/15</p>
+      <p>Questioons</p>
+     
+    </main>
+  )
+}
+
+export default Main
